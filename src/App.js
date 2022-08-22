d1dd2685b911b12b64f627432334aa1959d3adc9
Ramazan Alp <rmznalp@gmail.com>
2022-08-22 15:34:10 +0100

    adding search function

--- src/App.js
+++ src/App.js
@@ -3,21 +3,33 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import axios from 'axios';
+import Search from './components/users/Search';
 class App extends Component {
 	state = {
 		users: [],
 		loading: false,
 	};
-	async componentDidMount() {
+	// async componentDidMount() {
+	// 	this.setState({ loading: true });
+	// 	const res = await axios.get('https://api.github.com/users');
+	// 	this.setState({ users: res.data, loading: false });
+	// }
+
+	searchUsers = async (text) => {
 		this.setState({ loading: true });
-		const res = await axios.get('https://api.github.com/users');
-		this.setState({ users: res.data, loading: false });
-	}
+		console.log(text);
+		const res = await axios.get(
+			`https://api.github.com/search/users?q=${text}`
+		);
+		this.setState({ users: res.data.items, loading: false });
+	};
+
 	render() {
 		return (
 			<div className='App'>
 				<Navbar title='Github Finder' />
 				<div className='container'>
+					<Search searchUsers={this.searchUsers} />
 					<Users users={this.state.users} loading={this.state.loading} />
 				</div>
 			</div>
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 import UserItem from './UserItem';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
 
-const Users = (props) => {
+const Users = ({ users, loading }) => {
+	if (loading) {
+		return <Spinner />;
+	}
 	return (
 		<div style={userStyle}>
-			{props.users.map((user) => {
+			{users.map((user) => {
 				return <UserItem key={user.id} user={user} />;
 			})}
 		</div>
 	);
+};
+
+Users.propTypes = {
+	users: PropTypes.array.isRequired,
+	loading: PropTypes.bool.isRequired,
 };
 
 const userStyle = {
